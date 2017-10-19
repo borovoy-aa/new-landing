8a0bcc6daa7842f6de9475a4a58d562e7240324d
Artem Borovoy <work.borovoy@gmail.com>
2017-10-19 18:16:25 +0300

    'new-shpek'

---
+++ build/js/side-list.js
@@ -0,0 +1,7 @@
+// $(document).ready(function(){ 
+//     var active = "is-active";
+//         $("#menu .list-tab").on("click", function(){ 
+//         $("#menu .list-tab." + active).removeClass(active); 
+//         $(this).addClass(active); 
+//     }); 
+// });
